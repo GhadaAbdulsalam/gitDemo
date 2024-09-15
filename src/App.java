a54d33f785ea490ad9a224a3084494d57c850a54
Ghada <00ghada@gmail.com>
2024-09-15 13:56:43 +0300

    A change from the first device

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Hello, F56 Section!");
-        System.out.println("A change from second device!");
+        System.out.println("A change from the first device!");
     }
 }
