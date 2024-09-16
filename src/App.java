1a48aefbef60663bc8df759f707a9b3818522c48
Ghada <00ghada@gmail.com>
2024-09-16 15:42:21 +0300

    initial commit

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("Hello, F56 Section!");
-        System.out.println("A change from the first device!");
     }
 }
